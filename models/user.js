cc940d67a3403c90e8a7fa6b6f0871054261c444
nickhendren <nhendren7@gmail.com>
2015-11-14 15:18:59 -0800

    created a User model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,22 @@
 var mongoose = require('mongoose');
 
 //Nick will setup
+
+//creates a user schema
+var userSchema = new mongoose.Schema({
+  user_name: String,
+  email: {type: String, require: true, unique: true},
+  name: String,
+  age: Number
+})
+
+//creates a new method to display the user's info in the console
+userSchema.methods.info = function(){
+  console.log('My name is ' + this.name + ' and I am ' + this.age + ' years old. My user name is ' + this.user_name + '.')
+}
+
+//creates a new variable for the user schema
+var User = mongoose.model('User', userSchema)
+
+//exports the model module to use in users_controller.js
+module.exports = User
