2f7dbd0e96d0d4bfeb3b4c4582ec131c7ee01d33
Kevin <kevinjustinwilliams@gmail.com>
2015-11-13 17:07:37 -0800

    added app.js info

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    logger = require('morgan'),
     app = express(),
     mongoose = require('mongoose')
+
+
+//add router with express
+
+
+app.listen(3000)
+
+//call the router
