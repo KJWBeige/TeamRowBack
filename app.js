7e0b6ecd6142ebf63ecd3fbb0ec7490b09d11f6a
bfrager <btfrager@gmail.com>
2015-11-16 01:20:38 -0800

    added controller structure and controller module exports + app.js routes, logs, mongodb connection and json bodyparser

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-//Sam does this
+// Sam does this
 
 var express = require('express'),
     bodyParser = require('body-parser'),
     logger = require('morgan'),
     app = express(),
-    mongoose = require('mongoose')
+    mongoose = require('mongoose'),
+    port 		= process.env.PORT || 3000,
+    userRoutes 	= require('./routes/user_routes.js')
 
+// establish connection to mongo database
+mongoose.connect('mongodb://localhost/users')
 
-//add router with express
+// logs requests
+app.use(logger('dev'))
 
+// makes json objects available in requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.listen(3000)
+// root route
+app.get('/', function(req, res) {
+	res.send("You're in the matrix")
+})
 
-//call the router
+// user routes
+app.use('/users', userRoutes)
+
+// set port
+app.listen(port)
+console.log('Server started on', port)
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,14 @@
 //Brian will setup "structure" controller
+var User = require('../models/user.js')
+var mongoose = require('mongoose')
+
+//display all users
+function index(req, res) {
+	User.find({}, function(err, users){
+		if (err) console.log(err)
+		res.json(users)
+	})
+}
 
 //create - Nick
 function create(req, res){
@@ -14,8 +24,20 @@ function create(req, res){
   })
 }
 //read - Sam
+function show(req, res) {
+	User.find({email: req.params.email}, function(err, user){
+		if (err) console.log(err)
+		res.json(user)
+	})
+}
 
 //update - Brian
+function update(req, res) {
+	User.findOneAndUpdate({email: req.params.email}, {age: req.body.age}, function(err, user){
+		if (err) console.log(err)
+		res.json(user)
+	})
+}
 
 //destroy - Kevin
 function destroy (req, res){
@@ -26,6 +48,9 @@ function destroy (req, res){
 }
 
 module.exports = {
-
-  ,destroy: destroy
+  index : index
+, create: create
+, show: show
+, update: update
+, destroy: destroy
 }
