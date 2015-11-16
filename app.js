9447e13ad63bb47ad1d586290934bfa7bfa2007b
sam <samrichardson01@hotmail.com>
2015-11-16 13:04:53 -0800

    added about page

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ var express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
     port 		= process.env.PORT || 3000,
-    userRoutes 	= require('./routes/user_routes.js')
+    userRoutes 	= require('./routes/user_routes.js'),
+    ejs         = require('ejs'),
+    ejsLayouts  = require('express-ejs-layouts')
+
+    app.set('view engine', 'ejs')
+
+    app.use(ejsLayouts)
 
 // establish connection to mongo database
 mongoose.connect('mongodb://localhost/users')
@@ -22,6 +28,25 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.get('/', function(req, res) {
 	res.send("You're in the matrix")
 })
+
+
+app.get(('/about'), function(req, res){
+ var collection2 = [
+     {name: "About1", email: "lily@gmail.com"},
+     {name: "About2", email: "ken@gmail.com"},
+     {name: "About3", email: "kate@gmail.com"},
+ ]
+ //so below, you can direclty type into the 2nd argument what you want to view
+ //OR you can create a variable, etc
+ var body2 = {
+   title: "The About Page",
+   message: "Boom Boom Boom!",
+   users: collection2
+ }
+ res.render('about', body2)
+})
+
+
 
 // user routes
 app.use('/users', userRoutes)
